85017bc69a3a49bb26d992a65a64bf22a4d6b7f8
Haresh <lakhwaniharesh134@gmail.com>
2019-11-26 08:09:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlindownloadmanger/DownloadReceiver.kt
@@ -0,0 +1,28 @@
+package com.example.kotlindownloadmanger
+
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class DownloadReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0)
+        if (id !in DownloadManagerHelper.getDownloads(context)) {
+            return
+        }
+
+        val file = DownloadManagerHelper.getDownloadedFile(context, id)
+
+        Toast.makeText(context, "$id: ${file.statusText}", Toast.LENGTH_SHORT).show()
+
+        DownloadManagerHelper.removeDownload(context, id)
+
+        if (file.isSuccessful()) {
+            context.startService(DownloadIntentService.create(context, file))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kotlindownloadmanger/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.kotlindownloadmanger
+
+
+import android.app.DownloadManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+ import android.text.method.ScrollingMovementMethod
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+
+    private val largeURL = "https://upload.wikimedia.org/wikipedia/commons/a/a0/%27Greeley_Panorama%27_from_Opportunity%27s_Fifth_Martian_Winter%2C_PIA15689.jpg"
+    private val smallURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+
+    private val noCache: String
+        get() {
+            return "?ts=" + System.currentTimeMillis()
+        }
+
+    private var downloadManager: DownloadManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        downloadLargeImage.setOnClickListener {
+            download("Large", null, largeURL + noCache)
+        }
+
+        downloadSmallImage.setOnClickListener {
+            download("Small", null, smallURL + noCache)
+        }
+
+        viewDownloads.setOnClickListener {
+            val i = Intent()
+            i.action = DownloadManager.ACTION_VIEW_DOWNLOADS
+            startActivity(i)
+        }
+
+        monitor.movementMethod = ScrollingMovementMethod()
+
+        monitor.setOnLongClickListener {
+            monitor.text = ""
+            return@setOnLongClickListener true
+        }
+    }
+
+    private fun download(title: String, description: String?, uri: String) {
+
+        val request = DownloadManager.Request(Uri.parse(uri))
+
+        val appName = getString(R.string.app_name)
+        request.setTitle("$appName: $title")
+
+        description?.let {
+            request.setDescription(description)
+        }
+
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+
+        request.setVisibleInDownloadsUi(true)
+
+        val id = downloadManager?.enqueue(request) ?: -1L
+        if (id == -1L) {
+            return
+        }
+
+        DownloadManagerHelper.saveDownload(this, id)
+
+        checkDownload(id)
+    }
+
+    private fun checkDownload(id: Long) {
+        downloadManager?.let {
+
+            val file = DownloadManagerHelper.getDownloadedFile(this, id)
+
+            monitor.append(file.toString() + "\n\n")
+
+            if (id in DownloadManagerHelper.getDownloads(this)) {
+                Handler().postDelayed({ checkDownload(id) }, 500)
+            }
+        }
+    }
+}
